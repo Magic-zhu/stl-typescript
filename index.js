85619a0c88170cd1331dd7d2ea35f26bd89a5835
magiczhu <18817313320@163.com>
2020-01-31 15:24:15 +0800

    feat:项目初始化

---
+++ index.js
@@ -0,0 +1,11 @@
+import "./src/check"
+import "./src/typeOf"
+import './src/chunk'
+
+const helper = {
+    check,
+    chunk,
+    typeOf,
+}
+
+export default helper
---
+++ src/typeOf.js
@@ -0,0 +1,38 @@
+
+/**
+ * @description 检查某个数据的数据类型
+ * |输入值|输出
+ * |---|---|
+ * |123|Number
+ * |'abcdef'|String
+ * |true|Bollean
+ * |[1, 2, 3, 4]|Array
+ * |{name:'wenzi', age:25}|Object
+ * |console.log('this is function')|Function
+ * |undefined|Undefined
+ * |null|Null
+ * |new Date()|Date
+ * |/^[a-zA-Z]{5,20}$/|RegExp
+ * |new Error()|Error
+ * @param {*} value - 输入值
+ * @param {String} [type] - 需要核对的数据类型，不填的时候则返回数据类型
+ * @return {Boolean|String} - 返回首字母大写的数据类型（ex：Number）或者布尔值
+ * @version 1.0.0
+ */
+const typeOf = function(value,type){
+    let r = typeof value
+    if (r !== 'object') {
+        if(type){
+            return r.charAt(0).toUpperCase() + r.slice(1,r.length) == type
+        }else{
+            return r.charAt(0).toUpperCase() + r.slice(1,r.length)
+        }
+    }else{
+        if(type){
+            return Object.prototype.toString.call(value).replace(/^\[object (\S+)\]$/, '$1') == type
+        }else{
+            return Object.prototype.toString.call(value).replace(/^\[object (\S+)\]$/, '$1')
+        }
+    }
+}
+export default typeOf

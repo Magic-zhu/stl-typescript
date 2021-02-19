c693fbc5cb304f967993df545aad3ca43f926366
magic-zhu <18817313320@163.com>
2021-02-19 20:22:45 +0800

    feat:update

--- test/getStringByteLength.test.js
+++ test/getStringByteLength.test.js
@@ -1,4 +1,4 @@
-import getStringByteLength from "../src/string/getStringByteLength"
+import getStringByteLength from "../src/getStringByteLength"
 
 test("getStringByteLength(123hello酷儿)) should be 12",()=>{
     expect(getStringByteLength('123hello酷儿')).toBe(12)
---
+++ test/remainingTime.test.js
@@ -0,0 +1,7 @@
+import remainingTime from '../src/remainingTime'
+test('剩余时间', () => {
+    expect(remainingTime(new Date().getTime() + 60000).day).toBe(0)
+    expect(remainingTime(new Date().getTime() + 60000).time).toBe("00:01:00")
+    expect(remainingTime(new Date().getTime() + 3600000).time).toBe("01:00:00")
+    expect(remainingTime(new Date().getTime() + 86400000).day).toBe(1)
+})

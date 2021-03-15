aee5d4d322c25ac0b101f76959bb516d7739e6b4
magic-zhu <18817313320@163.com>
2021-03-15 15:56:13 +0800

    feat:update

---
+++ index.js
@@ -0,0 +1,27 @@
+import chunk from "./src/chunk"
+import check from "./src/check"
+import copy from "./src/copy"
+import debounce from "./src/debounce"
+import formatTime from "./src/formatTime"
+import getQuery from "./src/getQuery"
+import now from './src/now'
+import Observer from "./src/Observer"
+import throttle from "./src/throttle"
+import typeOf from "./src/typeOf"
+import shuffle from "./src/shuffle"
+
+const helper = {
+    check,
+    chunk,
+    copy,
+    debounce,
+    formatTime,
+    getQuery,
+    now,
+    Observer,
+    throttle,
+    typeOf,
+    shuffle,
+}
+
+export default helper

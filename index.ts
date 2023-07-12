9d95b1f6356ef21982202a1b2b49b5675c940ef5
magic-zhu <18817313320@163.com>
2023-07-12 20:24:49 +0800

    :sparkles:

--- index.ts
+++ index.ts
@@ -1,26 +1,65 @@
-export * from "./src/chunk"
-export * from "./src/check"
-export * from "./src/getQuery"
-export * from "./src/getStringByteLength"
-export * from "./src/IosOrAndroid"
-export * from "./src/shuffle"
-export * from "./src/sliceByByte"
-export * from "./src/solveIosKeybordBug"
-export * from "./src/string/blobToString"
-export * from "./src/IO"
+import {chunk} from "./src/chunk"
+import {check} from "./src/check"
+import {getQuery} from "./src/getQuery"
+import {getStringByteLength} from "./src/getStringByteLength"
+import {IosOrAndroid} from "./src/IosOrAndroid"
+import {shuffle} from "./src/shuffle"
+import {sliceByByte} from "./src/sliceByByte"
+import {solveIosKeybordBug} from "./src/solveIosKeybordBug"
+import {blobToString} from "./src/string/blobToString"
+import IO from "./src/IO"
+
 // ? time
-export * from "./src/time/getFpsTime"
-export * from "./src/time/now"
-export * from "./src/time/remainingTime"
-export * from "./src/time/formatTime"
+import {getFpsTime} from "./src/time/getFpsTime"
+import {now} from "./src/time/now"
+import {remainingTime} from "./src/time/remainingTime"
+import {formatTime} from "./src/time/formatTime"
+
 // ? utils
-export * from "./src/utils/throttle"
-export * from "./src/utils/typeOf"
-export * from "./src/utils/os"
-export * from "./src/utils/copy"
-export * from "./src/utils/debounce"
+import {throttle} from "./src/utils/throttle"
+import {typeOf} from "./src/utils/typeOf"
+import {os} from "./src/utils/os"
+import {copy} from "./src/utils/copy"
+import {debounce} from "./src/utils/debounce"
+
 // ? geometry
-export * from "./src/geometry/intersection_rectangle"
+import {intersection_rectangle} from "./src/geometry/intersection_rectangle"
+import {Box3} from "./src/geometry/Box3"
+import {Sphere} from "./src/geometry/Sphere"
+import {Triangle} from "./src/geometry/Triangle"
+
 // ? math
-export * from "./src/math/Vector3"
-export * from "./src/math/Lerp"
+import {Vector3} from "./src/math/Vector3"
+import {lerp, lerpNumber, lerpVector3, lerpVector3Ease} from "./src/math/Lerp"
+import {Vector2} from "./src/math/Vector2"
+import {Vector4} from "./src/math/Vector4"
+
+export {
+    chunk,
+    check,
+    getQuery,
+    getStringByteLength,
+    IosOrAndroid,
+    shuffle,
+    sliceByByte,
+    solveIosKeybordBug,
+    blobToString,
+    Vector3,
+    lerp, lerpNumber, lerpVector3, lerpVector3Ease,
+    intersection_rectangle,
+    IO,
+    getFpsTime,
+    now,
+    remainingTime,
+    formatTime,
+    throttle,
+    typeOf,
+    os,
+    copy,
+    debounce,
+    Box3,
+    Sphere,
+    Triangle,
+    Vector2,
+    Vector4,
+}
--- src/time/now.ts
+++ src/time/now.ts
@@ -1,10 +1,15 @@
 /**
- * @description 
+ * @description
  * <span style='color:red'>获取到目前为止的毫秒数</span>
  * @return {Number} - 返回到目前为止的毫秒数
  * @version 1.0.0
+ * @param type - ms:返回毫秒时间戳 s:返回秒的时间戳
  */
-export const now = function now (){
+export const now = function (type?: string) {
     let date = new Date()
-    return date.getTime()
+    if (!type || type === 'ms') {
+        return date.getTime()
+    } else if (type === 's') {
+        return Math.round(date.getTime() / 1000)
+    }
 }

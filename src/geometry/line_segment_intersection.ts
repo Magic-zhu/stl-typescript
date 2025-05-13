cefe39c6976210d4f4b7fbe48c87e554648f6e3f
Alex Goot <18817313320@163.com>
2025-05-13 13:50:46 +0800

    feat: 新增文件处理工具和数组遍历函数
    新增了文件处理工具 `file.ts` 和数组遍历函数 `IForEach.ts`，分别用于获取文件后缀和实现可中断的数组遍历功能。同时，添加了 `.vscode/settings.json` 配置文件以支持 Conventional Commits 风格的提交信息。

--- src/geometry/line_segment_intersection.ts
+++
@@ -1,5 +0,0 @@
-import { GPU } from "gpu.js";
-import { Vector3 } from "../math/Vector3";
-export function line_segment_intersection(a, b, c, d): boolean {
-  return false;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,49 @@
-import { chunk } from "./chunk"
-import { check } from "./check"
-import { getQuery } from "./getQuery"
-import { getStringByteLength } from "./getStringByteLength"
-import { IosOrAndroid } from "./IosOrAndroid"
-import { shuffle } from "./shuffle"
-import { sliceByByte } from "./sliceByByte"
-import { solveIosKeybordBug } from "./solveIosKeybordBug"
-import { blobToString } from "./string/blobToString"
-import IO from "./IO"
+import { chunk } from "./chunk";
+import { check } from "./check";
+import { getQuery } from "./getQuery";
+import { getStringByteLength } from "./getStringByteLength";
+import { IosOrAndroid } from "./IosOrAndroid";
+import { shuffle } from "./shuffle";
+import { sliceByByte } from "./sliceByByte";
+import { solveIosKeybordBug } from "./solveIosKeybordBug";
+import { blobToString } from "./string/blobToString";
+import IO from "./IO";
 
 // ? array
 
-import { ArrayPro } from "./array/ArrayPro"
+import { ArrayPro } from "./array/ArrayPro";
+import { IForEach } from "./array/IForEach";
 // import { SearchContainer } from "./array/SearchContainer";
 
 // ? time
-import { getFpsTime } from "./time/getFpsTime"
-import { now } from "./time/now"
-import { remainingTime } from "./time/remainingTime"
-import { formatTime } from "./time/formatTime"
+import { getFpsTime } from "./time/getFpsTime";
+import { now } from "./time/now";
+import { remainingTime } from "./time/remainingTime";
+import { formatTime } from "./time/formatTime";
 
 // ? utils
-import { throttle } from "./utils/throttle"
-import { typeOf } from "./utils/typeOf"
-import { os } from "./utils/os"
-import { copy } from "./utils/copy"
-import { debounce } from "./utils/debounce"
-import { hexToRgb } from "./utils/color"
+import { throttle } from "./utils/throttle";
+import { typeOf } from "./utils/typeOf";
+import { os } from "./utils/os";
+import { copy } from "./utils/copy";
+import { debounce } from "./utils/debounce";
+import { hexToRgb } from "./utils/color";
 
 // ? geometry
-import { intersection_rectangle } from "./geometry/intersection_rectangle"
-import { Box3 } from "./geometry/Box3"
-import { Sphere } from "./geometry/Sphere"
-import { Triangle } from "./geometry/Triangle"
-import { Plane } from "./geometry/Plane"
+import { intersection_rectangle } from "./geometry/intersection_rectangle";
+import { Box3 } from "./geometry/Box3";
+import { Sphere } from "./geometry/Sphere";
+import { Triangle } from "./geometry/Triangle";
+import { Plane } from "./geometry/Plane";
 
 // ? math
-import { Vector3 } from "./math/Vector3"
-import { lerp, lerpNumber, lerpVector3, lerpVector3Ease } from "./math/Lerp"
-import { Vector2 } from "./math/Vector2"
-import { Vector4 } from "./math/Vector4"
-import { Matrix3 } from "./math/Matrix3"
-import { uuid } from "./math/uuid"
-import { euclideanModulo } from "./math/mod"
+import { Vector3 } from "./math/Vector3";
+import { lerp, lerpNumber, lerpVector3, lerpVector3Ease } from "./math/Lerp";
+import { Vector2 } from "./math/Vector2";
+import { Vector4 } from "./math/Vector4";
+import { Matrix3 } from "./math/Matrix3";
+import { uuid } from "./math/uuid";
+import { euclideanModulo } from "./math/mod";
 
 export {
   chunk,
@@ -50,35 +51,36 @@ export {
   getQuery,
   getStringByteLength,
   IosOrAndroid,
-  shuffle,
-  sliceByByte,
-  solveIosKeybordBug,
-  blobToString,
+  ArrayPro,
+  Box3,
+  IO,
+  IForEach,
+  Matrix3,
+  Plane,
+  Sphere,
+  Triangle,
+  Vector2,
   Vector3,
+  Vector4,
+  blobToString,
+  copy,
+  debounce,
+  euclideanModulo,
+  formatTime,
+  getFpsTime,
+  hexToRgb,
+  intersection_rectangle,
   lerp,
   lerpNumber,
   lerpVector3,
   lerpVector3Ease,
-  intersection_rectangle,
-  IO,
-  getFpsTime,
   now,
+  os,
   remainingTime,
-  formatTime,
+  shuffle,
+  sliceByByte,
+  solveIosKeybordBug,
   throttle,
   typeOf,
-  os,
-  copy,
-  debounce,
-  Box3,
-  Sphere,
-  Triangle,
-  Vector2,
-  Vector4,
-  Plane,
-  Matrix3,
-  ArrayPro,
-  hexToRgb,
   uuid,
-  euclideanModulo,
-}
+};
